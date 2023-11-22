1101b02e7d29b5b8568a8ccc5a4cdfa6f8f48f81
Noam Afergan <ifrganvrcturh@gmail.com>
2023-11-22 17:41:07 +0200

    Build 'Point' class that descript point

---
+++ Point.java
@@ -0,0 +1,26 @@
+
+public class Point {
+	private int _x;
+	private int _y;
+	
+	Point(int x, int y) {
+		this._x = x;
+		this._y = y;
+	}
+	
+	public int getX() {
+		return this._x;
+	}
+	
+	public int getY() {
+		return this._y;
+	}
+	
+	public void setX(int x) {
+		this._x = x;
+	}
+	
+	public void setY(int y) {
+		this._y = y;
+	}
+}
