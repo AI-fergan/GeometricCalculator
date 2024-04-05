2e68cb951ebc7d12f31a59d1c54a0413bc7593f5
AI-fergan <ifrganvrcturh@gmail.com>
2024-04-05 16:39:12 +0300

    Added open screen menu

--- Main.java
+++ Main.java
@@ -2,34 +2,16 @@ import java.util.LinkedList;
 import java.util.Scanner;
 public class Main {
 
-	public static void main(String[] args) {
-		Scanner in = new Scanner(System.in);
+	public static void main(String[] args) {		
 		LinkedList<Shape> shapes = new LinkedList<Shape>();
-		int option;
-		System.out.println("------------------------\n"
-						 + "| Geometric Calculator |\n"
-				    	 + "------------------------");
-		
-		System.out.println("1. Add Shape\n"
-						 + "2. Remove Shape\n"
-						 + "3. Calc Shape\n"
-						 + "4. Print all shapes\n"
-						 + "5. Exit");
 		
 		do {
-			System.out.print("option: ");
-			option = in.nextInt();
-			
-			switch(option) {
-				case 1:
-					System.out.flush();
-					System.out.println("shapes:");
-					System.out.println("1. point");
-					System.out.println("2. circle");
-					System.out.print("option: ");
-					option = in.nextInt();
-					switch(option) {
+			System.out.flush();
+			switch(Menu.openScreen()) {
+				case 1:				
+					switch(Menu.shapesMenu()) {
 						case 1:
+							shapes.add(new Circle());
 					}
 					break;
 				case 2:
--- Menu.java
+++ Menu.java
@@ -1,4 +1,32 @@
+import java.util.Scanner;
 
 public class Menu {
+	static Scanner in = new Scanner(System.in);
+	
+	public static int openScreen() {		
+		System.out.println(" ----------------------\n"
+				 	   	 + "| Geometric Calculator |\n"
+				 		 + " ----------------------");
 
+		System.out.println("1. Add Shape\n"
+						 + "2. Remove Shape\n"
+						 + "3. Calc Shape\n"
+						 + "4. Print all shapes\n"
+						 + "5. Exit");
+		
+		System.out.print("option: ");
+		
+		return in.nextInt();
+	}
+	
+	public static int shapesMenu() {
+		int option = 0;
+		System.out.println("shapes:");
+		System.out.println("1. point");
+		System.out.println("2. circle");
+		
+		System.out.print("3. option: ");
+		
+		return in.nextInt();		
+	}
 }
